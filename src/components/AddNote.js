05d08ba37b66475dc2184e4c5baee4d6ab744c8b
joeneedum <joeneedum@gmail.com>
2022-02-06 22:51:43 -0500

    commit

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 
-const AddNote = () => {
+const AddNote = ({ handleAddNote }) => {
+    const [noteText, setNoteText] = useState('');
+
+    const handleChange = (event) => {
+        setNoteText(event.target.value);
+    };
+
+    const handleSaveClick = () => {
+        if(noteText.trim().length > 0){
+            handleAddNote(noteText);
+            setNoteText('');
+        }
+    };
+
     return (
         <div className='note new'>
             <textarea
             rows='8'
             cols='10'
+            value={noteText}
             placeholder='Type to add a note...'
+            onChange={handleChange}
             ></textarea>
             <div className='note-footer'>
                 <small>200 Remaining</small>
-                <button className='save'>Save</button>
+                <button onClick={handleSaveClick} className='save'>Save</button>
             </div>
         </div>
     )
